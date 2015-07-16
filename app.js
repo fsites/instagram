04486080bc0b7add1b3f7dd36a3f7a3161e256a9
fsites <fsites521@gmail.com>
2015-07-15 19:54:34 -0700

    got client id and basic api stuff

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ angular.module('instagramApp', ['ngAnimate'])
 	.controller('MyCtrl', ['$scope', function($scope) {
 
 		$scope.onSubmit = function() {
-	
+			$scope.displayText = $scope.userInput;
+			$scope.userInput = "";
 		}
 
 	}]);
+
+
+
+//CLIENT ID	fe58bbb1a5724d1395b66b3f3728d11c
+//CLIENT SECRET	3e8f8e8bc17747dea0f6b789270d60af
